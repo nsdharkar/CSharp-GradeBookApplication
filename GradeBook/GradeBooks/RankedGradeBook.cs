5a54a7ba984e3f573459dc1261a43a430bd661d7
nsdharkar <nishantdharkar@gmail.com>
2020-04-24 00:00:18 -0700

    GetLetterGrade method override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,22 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            else if (averageGrade < 100 && averageGrade > 80)
+                return 'A';
+            else if (averageGrade < 80 && averageGrade > 60)
+                return 'B';
+            else if (averageGrade < 60 && averageGrade > 40)
+                return 'C';
+            else if (averageGrade < 40 && averageGrade > 20)
+                return 'B';
+            else
+                return 'F';
+        }
+
     }
 }
